1d648cba1692c7ccc0c0bcb50088c7003ac4ba6c
Pierre C <pierrechevallier@outlook.com>
2019-10-22 22:01:40 +0200

    Updating contact page

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import classNames from "clsx";
+
+class Contact extends Component {
+  render() {
+    return (
+      <div className={classNames("c-about", "o-fc-aic")}>
+        <h1>
+          This page is unfortunately in construction !
+        </h1>
+      </div>
+    );
+  }
+}
+
+export default Contact;
--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import Helmet from "react-helmet";
 import Layout from "../layout";
-import About from "../components/About/About";
+import Contact from "../components/Contact/Contact";
 import classNames from "clsx";
 
 class ContactPage extends Component {
@@ -10,7 +10,7 @@ class ContactPage extends Component {
       <Layout>
         <div className={classNames("c-about", "t-bg-white","u-p-l-l", "u-p-r-l", "u-border-r-normal-black", "u-border-l-light-black")}>
           <Helmet title={`Contact`} />
-          <About />
+          <Contact />
         </div>
       </Layout>
     );
