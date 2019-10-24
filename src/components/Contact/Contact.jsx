642ea1674172579904279d2e4c0d562302b64a6f
Pierre C <pierrechevallier@outlook.com>
2019-10-24 21:20:45 +0200

    Improved display for mobile and added contact form

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
 import classNames from "clsx";
+import FormComponent from "./Form";
 
 class Contact extends Component {
   render() {
     return (
       <div className={classNames("c-about", "o-fc-aic")}>
         <h1>
-          This page is unfortunately in construction !
+          Contact us
         </h1>
+        <FormComponent/>
       </div>
     );
   }
---
+++ src/components/Contact/Form.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import classNames from "clsx";
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+
+/**
+ * A contact form
+ */
+
+const Form = () => {
+    const [values, setValues] = React.useState({
+        name: '',
+        email: '',
+    });
+    const handleChange = name => event => {
+        setValues({ ...values, [name]: event.target.value });
+    };
+
+    return <form id="contact-form" className={classNames("o-fc-aic", "u-p-l", "c-contact-form_width")} onSubmit={() => {}} method="POST">
+        <div className={classNames("o-fc-js", "u-width-hundred")}>
+            <TextField
+                id="outlined-name"
+                label="Name"
+                value={values.name}
+                onChange={handleChange('name')}
+                margin="normal"
+                variant="outlined"
+            />
+        </div>
+        <div className={classNames("o-fc-js", "u-width-hundred")}>
+            <TextField
+                id="outlined-email-input"
+                label="Email"
+                type="email"
+                name="email"
+                autoComplete="email"
+                margin="normal"
+                variant="outlined"
+            />
+        </div>
+        <div className={classNames("o-fc-js", "u-m-b-m", "u-width-hundred")}>
+            <TextField
+                id="outlined-dense-multiline"
+                label="Message"
+                margin="normal"
+                variant="outlined"
+                multiline
+                rows="5"
+            />
+        </div>
+        <Button variant="contained" color="primary">Submit</Button>
+    </form>
+};
+
+export default Form;
