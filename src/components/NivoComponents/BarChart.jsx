6bfaf5fd91338924ebdacf2c9e21b314e443d14c
Pierre C <pierrechevallier@outlook.com>
2020-01-22 19:09:57 +0100

    Updating the chart and the css associated

--- src/components/NivoComponents/BarChart.jsx
+++ src/components/NivoComponents/BarChart.jsx
@@ -10,7 +10,7 @@ const MyResponsiveBar = ({ data}) => (
         data={data}
         keys={['Minutes difference']}
         indexBy="ID"
-        margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
+        margin={{ top: 50, right: 20, bottom: 50, left: 20 }}
         padding={0.3}
         colors={{ scheme: 'accent' }}
         defs={[
@@ -43,7 +43,8 @@ const MyResponsiveBar = ({ data}) => (
             tickRotation: 0,
             legend: 'Flight number',
             legendPosition: 'middle',
-            legendOffset: 32
+            legendOffset: 32,
+            tickRotation: 90
         }}
         axisLeft={{
             tickSize: 5,
@@ -56,30 +57,30 @@ const MyResponsiveBar = ({ data}) => (
         labelSkipWidth={12}
         labelSkipHeight={12}
         labelTextColor={{ from: 'color', modifiers: [ [ 'darker', 1.6 ] ] }}
-        legends={[
-            {
-                dataFrom: 'keys',
-                anchor: 'bottom-right',
-                direction: 'column',
-                justify: false,
-                translateX: 120,
-                translateY: 0,
-                itemsSpacing: 2,
-                itemWidth: 100,
-                itemHeight: 20,
-                itemDirection: 'left-to-right',
-                itemOpacity: 0.85,
-                symbolSize: 20,
-                effects: [
-                    {
-                        on: 'hover',
-                        style: {
-                            itemOpacity: 1
-                        }
-                    }
-                ]
-            }
-        ]}
+        // legends={[
+        //     {
+        //         dataFrom: 'keys',
+        //         anchor: 'bottom-right',
+        //         direction: 'column',
+        //         justify: false,
+        //         translateX: 120,
+        //         translateY: 0,
+        //         itemsSpacing: 2,
+        //         itemWidth: 100,
+        //         itemHeight: 20,
+        //         itemDirection: 'left-to-right',
+        //         itemOpacity: 0.85,
+        //         symbolSize: 20,
+        //         effects: [
+        //             {
+        //                 on: 'hover',
+        //                 style: {
+        //                     itemOpacity: 1
+        //                 }
+        //             }
+        //         ]
+        //     }
+        // ]}
         animate={true}
         motionStiffness={90}
         motionDamping={15}
